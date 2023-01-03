471d01749bb7010dfac6a956b977c4880029da1d
kukjinkim <tlaruddo2@gmail.com>
2023-01-02 20:23:17 -0800

    React Crash Course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react' // for class components
+import Header from './components/Header'
 
 function App() {
+  const name = 'Brad'
+  const x = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+
     </div>
   );
 }
+
+//class component
+// class App extends React.Component{
+//   render(){
+//     return (
+//       <h1> Hello</h1>
+//     )
+//   }
+// }
 
 export default App;
